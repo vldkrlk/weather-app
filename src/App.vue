4a2deaebdc125090854bc80f4286010590d91959
VladKoroliuk <koroliuk.work@gmail.com>
2022-04-17 18:31:44 +0300

    add preloader

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts">
 import PageMain from "./pages/page-main.vue";
+import AppPreloader from "./components/AppPreloader.vue";
 </script>
 <template>
   <Suspense>
     <template #default>
       <PageMain></PageMain>
     </template>
-    <template #fallback>Loading...</template>
+    <template #fallback>
+      <AppPreloader />
+    </template>
   </Suspense>
 </template>
